457bc79e3ff3c1840e26ba6308bfe32f6f8c9b98
WilliamRausch <nolanrausch@Gmail.com>
2017-08-23 12:15:11 -0400

    bug fixes

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var moment = require('moment');
 const chalk = require ('chalk');
  var now = moment().format("LLLL");
  var days = moment().format("DDDD");
- var seconds = moment().format("ss")
- var hours = moment().format("HH");
-var totalseconds = (seconds + (hours*3600));
+ var seconds = parseInt(moment().format("ss"));
+ var hours = parseInt(moment().format("HH"));
+ var minutes = parseInt(moment().format("MM"));
+var totalseconds = (seconds + (hours*3600)+(minutes*60));
 var month = moment().format("MM");
 var mday = moment().format("DD");
 var year = moment().format("YYYY");
+console.log(minutes);
  console.log("It is " + chalk.blue(now));
  console.log("It is the "+ chalk.keyword('pink')(days +"th")+ " day of the year.");
  console.log("it is "+ chalk.rgb(135, 206, 250)(totalseconds)+" seconds into the day.");
@@ -32,3 +34,5 @@ if(year%4==0){
 }else{
 	console.log("It " + chalk.red("is not") + " a leap year");
 }
+var something = moment().format("DD");
+console.log(something);
